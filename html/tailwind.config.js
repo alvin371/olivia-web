fd367776cce9db9e2f1b4bf7948fccb2de5d050f
Ways Alqorni <wayslos92@gmail.com>
2021-10-15 00:07:54 +0700

    sign in and sign up

---
+++ html/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color: {
+        'biru-olivia': '#3db2ff',
+        
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
